be7f42d950850c0cf9334bc1351b2c78d1684e74
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-05 20:56:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,9 +23,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create_with(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    Movie.new(m)
+    m.title = args[:title] if args != nil
   end
 end
 
