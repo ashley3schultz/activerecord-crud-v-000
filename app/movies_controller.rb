440d2dcc663abbd42b7ea0df6cf81b1af587e6b8
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-05 22:52:20 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,7 +27,7 @@ def can_be_created_in_a_block(args = {})
   Movie.create do |m|
     m.title = args[:title] if args != {}
     m.release_date = args[:release_date] if args != {}
-    m.title
+    m.title  if args != {}
   end
 end
 
