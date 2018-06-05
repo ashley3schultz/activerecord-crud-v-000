fb3e9968025f48b76faa92c314dc15d0a9b1f15f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-05 20:22:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = __
 end
 
-def can_be_created_in_a_block(args = {})
+def can_be_created_in_a_block(args = __)
   binding.pry
   Movie.create do |m|
     __
