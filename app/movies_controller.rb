65138918d239cf9db980149aeb63cb6c00668738
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-05 21:17:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -51,9 +51,7 @@ end
 
 def can_find_by_multiple_attributes
   movie = Movie.where(title: 'Title').AND(release_date: 2000).AND(director: 'Me')
-  movie.title
-  movie.release_date
-  ovie.director
+  movie
 end
 
 def can_find_using_where_clause_and_be_sorted
